8608f8f50f2f6aa7910489853d088137c6cf3815
Swati Gupta <65848172+SwatiInd@users.noreply.github.com>
2020-06-16 16:34:12 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,16 +1,19 @@
 library(lubridate)
 library(dplyr)
 
-# setwd <- "C:/Users/Swati Gupta/Documents/Online_Courses/Data Analysis R Foundations/Exploratory data analysis/Project 1/Project-1"
+#Downloading the data from url to temp file
 url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
 temp <- tempfile()
 download.file(url, temp)
 a <- read.delim(unzip(temp),header = TRUE, sep = ";", dec = ",")
-# a <- read.delim("./../household_power_consumption.txt", header = TRUE, sep = ";", dec = ",")
+#reading the downloaded data into temp file by unzipping it
+a <- read.delim(unzip(temp),header = TRUE, sep = ";", dec = ",", na.strings = "?")
 
 ## converting Date data into Date format and extracting data of two days
-a <- a %>% mutate(Date = dmy(Date)) %>%
+a <- a %>% mutate(Date = dmy(Date)) %>% 
           filter(Date %in% as.Date(c("2007-02-01", "2007-02-02"))) 
+
+#Data of all variables other than date and time are converted into numeric from characters
 a[,3:9] <- apply(a[,3:9], 2, as.numeric)
 
 # plotting global active power histogram in png file
