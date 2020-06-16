c2277b638b4916a2eb1e2fbc05a0f599ec8a3fae
Swati Gupta <65848172+SwatiInd@users.noreply.github.com>
2020-06-16 14:45:41 +0100

    Histogram plot
    Data is read from the wesite

--- plot1.R
+++ plot1.R
@@ -1,8 +1,12 @@
 library(lubridate)
 library(dplyr)
 
-setwd <- "C:/Users/Swati Gupta/Documents/Online_Courses/Data Analysis R Foundations/Exploratory data analysis/Project 1/Project-1"
-a <- read.delim("./../household_power_consumption.txt", header = TRUE, sep = ";", dec = ",")
+# setwd <- "C:/Users/Swati Gupta/Documents/Online_Courses/Data Analysis R Foundations/Exploratory data analysis/Project 1/Project-1"
+url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+temp <- tempfile()
+download.file(url, temp)
+a <- read.delim(unzip(temp),header = TRUE, sep = ";", dec = ",")
+# a <- read.delim("./../household_power_consumption.txt", header = TRUE, sep = ";", dec = ",")
 
 ## converting Date data into Date format and extracting data of two days
 a <- a %>% mutate(Date = dmy(Date)) %>%
